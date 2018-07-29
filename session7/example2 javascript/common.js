a663c1093cce3b150cf1a7bcae3c6ab6b08694ed
Tran Tu <trantu230399@gmail.com>
2018-07-29 08:13:22 +0700

    abc

---
+++ session7/example2 javascript/common.js
@@ -0,0 +1,7 @@
+//alert('hello javascript 1');
+document.write('hello body');
+document.getElementById('demo1').innerHTML='hello script 3';
+var a="10";
+a+=5;
+//dùng để kiểm tra dữ liệu
+console.log(a);
---
+++ session7/example3 javascript/common.js
@@ -0,0 +1,17 @@
+/*var a;
+var b = 5;
+var c,d,e;
+var userName;//camel
+var user_name;
+var name='tu';
+var age='18';
+var date='23-03-1999';
+var town='danang';
+//comment
+/*
+comment
+*/
+var a = 2;
+var b = 3;
+document.write('ket qua cua a+b la:');
+document.write(a+b);
